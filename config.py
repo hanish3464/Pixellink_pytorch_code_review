37449336f0fb4773a6bcabcc0120aabc4a36ee44
hanish <hanish-@naver.com>
2019-07-12 18:06:04 +0900

    staticmethod image preprocessing

--- config.py
+++ config.py
@@ -5,18 +5,19 @@ learning_rate = 1e-3
 learning_rate2 = 1e-2
 all_trains = 1000 #read_labels() 를 호출할때 num parameter의 argument에 해당함. 사진이 1000장 있으니 1000장 넘어간다.
 batch_size = 24 #read_image call을 해서 24번 불러옴
-momentum = 0.9
-weight_decay = 5e-4
+momentum = 0.9 #momentum은 gradient descent를 하는 과정에서 일종에 관성을 주는 방식으로 과거의 이동방식을 기억하는 것을 말한다.
+               #보통 0.9정도의 momentum term의 값을 사용한다.
+weight_decay = 5e-4 #regularization 의 parameter lamda라고 할 수 있다. 
 dilation = True
 use_crop = False
-use_rotate = True
+use_rotate = True #rotate 를 사용
 # iterations = 10
-gpu = True
+gpu = True #gpu 사용관련 parameter
 multi_gpu = True # only useful when gpu=True
 pixel_weight = 2
 link_weight = 1
 
-r_mean = 123.
+r_mean = 123. #data transform 을 하는 단계에서 사용하는 parameter
 g_mean = 117.
 b_mean = 104.
 
--- main.py
+++ main.py
@@ -55,17 +55,23 @@ def retrain():
 
 
 def train(epoch, iteration, dataloader, my_net, optimizer, optimizer2, device):
-    for i in range(epoch):
-        for i_batch, sample in enumerate(dataloader):
+    print("train() call")
+    for i in range(epoch): #epoch(전첸 training set 1000개를 한번도는 것) 6만번 전체6만번을 돌리겠다.
+        print("train() -> for(epoch)")
+        for i_batch, sample in enumerate(dataloader): #dataloader를 가지고 dataset을 로드하여 사용한다. enumerate에서
+            print("train()-> for(epoch)-> enumerate(dataloader)") #dataloader가 불려서 getitem을 부른다. 
+            #i_batch : index 에 해당. sample은 dataloader안에 들은 content에 해당한다.
+            print("sample {}".format(sample.keys())) #dataloader 안에 content로 dict가 존재한다.
+            #image, pixel_mask, neg_pixel_mask, label, pixel_pos_weight, link_mask 의 key들이 존재하며, 안에는 3차원의 값이 들어있다.
             start = time.time()
-            images = sample['image'].to(device)
+            images = sample['image'].to(device) #device에 연결된 GPU로 image연산을 맡긴 image 변수 생성.
             # print(images.shape, end=" ")
-            pixel_masks = sample['pixel_mask'].to(device)
+            pixel_masks = sample['pixel_mask'].to(device) #각각 마찬가지로 sample에 들어있는 키들 연ㅅ나 역시 GPU에 할당.
             neg_pixel_masks = sample['neg_pixel_mask'].to(device)
             link_masks = sample['link_mask'].to(device)
             pixel_pos_weights = sample['pixel_pos_weight'].to(device)
-
-            out_1, out_2 = my_net.forward(images)
+            print("before forward debug")
+            out_1, out_2 = my_net.forward(images) ##문제발생
             loss_instance = PixelLinkLoss()
             # print(out_2)
 
@@ -74,6 +80,7 @@ def train(epoch, iteration, dataloader, my_net, optimizer, optimizer2, device):
             link_loss_pos, link_loss_neg = loss_instance.link_loss(out_2, link_masks)
             link_loss = link_loss_pos + link_loss_neg
             losses = config.pixel_weight * pixel_loss + config.link_weight * link_loss
+            print("debug check after loss")
             print("iteration %d" % iteration, end=": ")
             print("pixel_loss: " + str(pixel_loss.tolist()), end=", ")
             # print("pixel_loss_pos: " + str(pixel_loss_pos.tolist()), end=", ")
@@ -107,27 +114,29 @@ def main():
     print("dataset type : {}\ndataset info : {}\n dataset len : {}".format(type(dataset), dataset, len(dataset)))
     sampler = WeightedRandomSampler([1/len(dataset)]*len(dataset), config.batch_size, replacement=True)
     print("sampler: {}".format(len(sampler)))
-    dataloader = DataLoader(dataset, batch_size=config.batch_size, sampler=sampler)
-    ## dataloader = DataLoader(dataset, config.batch_size, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=config.batch_size, sampler=sampler) #dataloader는 np의 array와 유사한  tensor로 저장.    ## dataloader = DataLoader(dataset, config.batch_size, shuffle=True)
     print("my_net call")
     my_net = net.Net()
-
+    
     if config.gpu:
-        device = torch.device("cuda:0")
-        my_net = my_net.cuda()
-        if config.multi_gpu:
-            my_net = nn.DataParallel(my_net)
+        device = torch.device("cuda:0") #CUDA는 NVIDIA에서 개발한 GPU사용을 위한 툴이라고 할 수 있다. #계산 device로 GPU를 쓰겠다.
+        my_net = my_net.cuda() #net같은 network 역시 GPU로 하겠다.
+        if config.multi_gpu: #GPU가 여러개 있다면,
+            my_net = nn.DataParallel(my_net) #parallel한 방식으로 나눠서 진행하겠다.
     else:
-        device = torch.device("cpu")
+        device = torch.device("cpu") #그렇지 않다면 계산 device를 CPU로 연결하겠다.
     print("config.gpu: check success!")
     ##nn.init.xavier_uniform_(list(my_net.parameters()))
-    my_net.apply(weight_init)
+    my_net.apply(weight_init) #network weight parameter를 initialize 한다.
     optimizer = optim.SGD(my_net.parameters(), lr=config.learning_rate, momentum=config.momentum, weight_decay=config.weight_decay)
+    #weight_decay : L2 regulization
     ##if args.change:
+    #Stochastic Gradient Descent(SGD)로 미니 배치사이즈를 이용해 gradient descent를 진행하며 optimization한다. 
+    print("parameter : {}".format(my_net.parameters()))
     optimizer2 = optim.SGD(my_net.parameters(), lr=config.learning_rate2, momentum=config.momentum, weight_decay=config.weight_decay)
     ##else:
     ##     optimizer2 = optim.SGD(my_net.parameters(), lr=config.learning_rate, momentum=config.momentum, weight_decay=config.weight_decay)
-
+    print("Go to train")
     iteration = 0
     train(config.epoch, iteration, dataloader, my_net, optimizer, optimizer2, device) #여기서 training 시작
 
